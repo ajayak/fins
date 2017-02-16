97d7047b2a1814f7c0d7c9d070df2804e1bf4c8d
Ajay Kumar <ajayak731@hotmail.com>
2017-02-16 16:10:05 +0530

    Added organizationModel changes migration

---
+++ src/FINS/Migrations/20170216103827_AddedOrganizationDataTypeLimits.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FINS.Migrations
+{
+    public partial class AddedOrganizationDataTypeLimits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Organization",
+                newName: "OrganizationId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WebUrl",
+                table: "Organization",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Summary",
+                table: "Organization",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrivacyPolicy",
+                table: "Organization",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Organization",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LogoUrl",
+                table: "Organization",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DescriptionHtml",
+                table: "Organization",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Organization",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Organization");
+
+            migrationBuilder.RenameColumn(
+                name: "OrganizationId",
+                table: "Organization",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WebUrl",
+                table: "Organization",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Summary",
+                table: "Organization",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrivacyPolicy",
+                table: "Organization",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Organization",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LogoUrl",
+                table: "Organization",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DescriptionHtml",
+                table: "Organization",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 2000,
+                oldNullable: true);
+        }
+    }
+}
